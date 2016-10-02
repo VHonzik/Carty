3f661fc91565ee60a05b7c1f9b05e0af6e39fc74
vaclav_honzik <honzik.vaclav@gmail.com>
2016-10-02 23:00:35 +0200

    Finishing up unit tests for coroutine queue

---
+++ CartyUnity/Assets/UnitTests/CQAddWait.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using CartyLib;
+using Testing;
+
+[IntegrationTest.DynamicTest("CartyLibTests")]
+public class CQAddWait : MonoBehaviour {
+
+    private CoroutineQueue queue = new CoroutineQueue();
+    private float TimeElapsed { get; set; }
+    private float wait = 1.5f;
+    private int UpdateCount { get; set; }
+
+    void Start()
+    {
+        queue.Start();
+        queue.AddWait(wait);
+        TimeElapsed = 0;
+        UpdateCount = 0;
+    }
+
+    void Update()
+    {
+        if (TimeElapsed < wait)
+        {
+            IntegrationTest.Assert(queue.Empty == false);
+        }
+        // One frame for starting coroutine and one for realizing the queue is empty
+        else if (TimeElapsed >= wait && UpdateCount == 2)
+        {
+            IntegrationTest.Assert(queue.Empty == true);
+            IntegrationTest.Pass(gameObject);
+        }
+
+        TimeElapsed += Time.deltaTime;
+        if (TimeElapsed >= wait) UpdateCount++;
+    }
+}
---
+++ CartyUnity/Assets/UnitTests/CQWaitUntil.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using CartyLib;
+
+[IntegrationTest.DynamicTest("CartyLibTests")]
+public class CQWaitUntil : MonoBehaviour {
+
+    private CoroutineQueue queue;
+    private float _wait = 1.5f;
+    private int UpdateCount { get; set; }
+    private float TimeElapsed { get; set; }
+    private bool _wait_until_done = false;
+
+    private IEnumerator AssertWaitUntilDone()
+    {
+        yield return StartCoroutine(queue.WaitUntilEmpty());
+        _wait_until_done = true;
+    }
+
+    void Start()
+    {
+        queue = new CoroutineQueue();
+        queue.Start();
+        queue.AddWait(1.5f);
+        UpdateCount = 0;
+        StartCoroutine(AssertWaitUntilDone());
+    }
+
+    void Update () {
+
+        // Starting queue + Realizing it's empty + Assert getting hit + ???
+        if(TimeElapsed >= _wait && UpdateCount == 4)
+        {
+            IntegrationTest.Assert(_wait_until_done == true);
+            IntegrationTest.Pass(gameObject);
+        }
+        else
+        {
+            IntegrationTest.Assert(_wait_until_done == false);
+        }
+
+        TimeElapsed += Time.deltaTime;
+        if (TimeElapsed >= _wait) UpdateCount++;
+    }
+}
