2f3c77ab2c400d143f9bcec024f75b509f708d50
vaclav_honzik <honzik.vaclav@gmail.com>
2016-10-06 23:23:39 +0200

    Adding CI script for unit tests Adding 1 unit test for CoroutineTree Implementing Detachable component

---
+++ run_tests.bat
@@ -0,0 +1,21 @@
+@ECHO Running unit tests...
+@ECHO OFF
+IF "%UNITY%"=="" (
+ECHO UNITY environment variable is not set. Exiting.
+EXIT /B 1
+)
+
+set TESTRESULTS=%cd%\TestResults
+
+start /WAIT %UNITY%\Unity.exe -batchmode -projectPath %cd%\CartyUnity -executeMethod UnityTest.Batch.RunIntegrationTests -testscenes=CartyLibTests -targetPlatform=StandaloneWindows -resultsFileDirectory=%TESTRESULTS%
+
+SET ERRNO=%ERRORLEVEL%
+
+IF %ERRNO% EQU 0 (
+RD /S /Q %TESTRESULTS%
+ECHO All tests passed!
+) ELSE (
+ECHO Some tests have not passed, generating report to %TESTRESULTS%.
+)
+
+EXIT /B %ERRNO%
